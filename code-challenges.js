b523a603a9952484a7ed9c262f8c931fb9d87e48
brianthomashammond <brianthomashammond@gmail.com>
2020-10-04 18:12:15 -0700

    forgot to make a branch and pushed finished answers here - trying to restore this to just the questions

--- code-challenges.js
+++ code-challenges.js
@@ -1,7 +1,5 @@
 // ASSESSMENT 5: JavaScript Coding Practical Questions
 
-console.log("\n")
-console.log("Week 5 Assessment - Java Script Code Challenges - Brian Thomas Hammond", "\n")
 
 // --------------------1) Create a function that takes in a string and returns a coded message. The coded message converts 'a' to 4, 'e' to 3, 'i' to 1, and 'o' to 0.
 
@@ -11,39 +9,13 @@ var secretCodeWord2 = "gobbledygook"
 // Expected output: "g0bbl3dyg00k"
 
 
-//Write a function that takes a string
-const l33tSp34k = (string) => {
-    //Split each letter of that string into an array
-    //Map over that array using a ternary operator to change a, e, and o to 3, 4, and 0 respectively
-    //Leave all other characters unchanged
-    //Join all characters in the array into a string
-    //Return that string
-    return string.split("").map( value => value === 'a' ? value = 4 : value === 'e' ? value = 3 : value === 'o' ? value = 0 : value).join("")
-}
-
-console.log("#1:")
-console.log(l33tSp34k(secretCodeWord1))// expect: "l4ck4d41s1c4l"
-console.log(l33tSp34k(secretCodeWord2), "\n")// expect: "g0bbl3dyg00k"
-
-
 
 // --------------------2) Create a function that takes in an array and returns all the words that contain the letter a.
 
 var arrayOfWords = ["Apple", "Banana", "Plum", "Cherry", "Kiwi", "Peach"]
 // Expected output: "Apple" "Banana" "Peach"
 
-//Write a function that takes an array
-const allAWords = (array) => {
-    //Filter over the elements of the argument array
-    //Convert each element to lowercase
-    //Include all elements that contain the character 'a'
-    //Join all elements into a string, placing a space between each element 
-    //Return the string
-    return array.filter( value => value.toLowerCase().includes('a')).join(" ")
-}
 
-console.log("#2:")
-console.log(allAWords(arrayOfWords), "\n")// expect: "Apple" "Banana" "Peach"
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
@@ -54,31 +26,3 @@ var hand2 = [5, 5, 3, 3, 4]
 var hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
-//Write a function that takes an array
-const isFullHouse = (argumentArray) => {
-    //Create an empty array to hold the ranks from the argument array
-    answerArray = []
-    //Map over the argument array to push a single instance of each rank to the answer array
-    argumentArray.map(value => answerArray.includes(value) ? "" : answerArray.push(value))
-    //If the answerArray does not have a length of 2 return false for the entire function
-    if (answerArray.length !== 2) {
-        return false
-    }
-    // //If not, map over the elements of the answerArray to execute the following expession twice: 
-    // //Filter through the argument array - if the elements match the element of the answer array at the map's current index exactly twice return true for that iteration of map
-    // //If either iteration of the map function returns true (i.e. if the array returned by map includes the value true), then return true for the entire function 
-    else if (answerArray.map((value, index) => argumentArray.filter( value => value === answerArray[index]).length === 2).includes(true)) {
-        return true
-    }
-    // //Else return false for the entire function
-    else {
-        return false
-    }
-    //Here is the above logic in ternary form:
-    // return answerArray.length !== 2 ? false :  answerArray.map((value, index) =>  argumentArray.filter( value1 => value1 === answerArray[0]).length === 2).includes(true) ? true : false
-}
-
-console.log("#3:")
-console.log(isFullHouse(hand1))// expect: true
-console.log(isFullHouse(hand2))// expect: false
-console.log(isFullHouse(hand3), "\n")// expect: false
--- rails-questions.md
+++ rails-questions.md
@@ -2,49 +2,23 @@
 
 1. What is the command to create a new Rails application with a Postgres database?
 
-rails generate new app_name -d postgresql
-
-(We've been using -T at the end of ours but my understanding is that is related to the default testing suite, not Postgres.)
 
 
 2. Explain each section of the following route:  get '/library/:id' => 'book#show'
-
-get - The http verb to read data.
-library - is appended to the url to navigate to that page of our site.
-:id - is also appended to the url in order to pass data to our site so it can be manipulated.
-book - refers to a controller named book.
-show - refers to a method in book that is being passed the data from :id in order to render the page. 
+ 
 
 
 3. Oops, I forgot to add a foreign key to my model. Describe the steps to remedy this mistake.
 
-For a model with classes Driver and Car:
-
-$ rails generate migration AddForeignKeyToCars
-
-In the migration file:
-
-def change
-    add_column :cars, :driver_id, :integer
-end
 
 
 4. What is the command to generate a Rails model for a person table with columns named shirt, pants, and shoes?
 
-rails generate model Person shirt:boolean pants:boolean shoes:boolean
 
 
 5. I want to add validations to the model from question 4 to ensure no one can make a new entry without data in all the columns. How do I do that?
 
-navigate to app/models/person.rb
-
-class Owner < ApplicationRecord
-    validates: :shirt, :pants, :shoes, presence: true
-end
 
 
 6. What is the naming convention for generating a Rails model? What is the naming convention for the table that is generated with this command?
 
-A rails model is singular and named using PascalCase.
-
-A table is plural and named using snake_case
